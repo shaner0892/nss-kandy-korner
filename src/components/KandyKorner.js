914718141381828e117c42beb99b7df22defc1dd
Hilary Shaner <hilaryshaner@gmail.com>
2022-02-24 16:01:09 -0600

    modules created, working on locations

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { LocationList } from "../locations/Locations";
+
+export const KandyKorner = () => {
+    return (
+        <>
+            <h1>Kandy Korner</h1>
+            <h2>Locations</h2>
+            <LocationList />
+        </>
+    )
+}
---
+++ src/locations/Locations.js
@@ -0,0 +1,30 @@
+// utilize the useEffect() React hook to get the array of locations from your API. All of this will be nearly identical to Honey Rae's
+import React, { useEffect, useState } from "react";
+
+export const LocationList = () => {
+    const [locations, modifyLocations] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8089/locations")
+                .then(res => res.json())
+                .then((locationArray) => {
+                    modifyLocations(locationArray)
+                })
+        },
+        []
+    )
+
+    return (
+        <h1>Locations</h1>
+
+        // {
+        //     locations.map(
+        //         (locationObject) => {
+        //             return <p key={`location--${locationObject.id}`}>{location.address}</p>
+        //         }
+        //     )
+        // }
+    )
+
+}
