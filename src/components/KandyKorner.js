2a6300b9619202a5199175da9a6343c7d2b459a4
Hilary Shaner <hilaryshaner@gmail.com>
2022-03-02 14:35:41 -0600

    end of ch 12, login page added

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,14 +1,34 @@
 import React from "react"
-// import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
 import { NavBar } from "./nav/NavBar"
 import { ApplicationViews } from "./ApplicationViews"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
 //this will be what is rendered in the DOM
-export const KandyKorner = () => {
-    return (
-        <>
-        <NavBar />
-        <ApplicationViews />
-        </>
-    )
-}
+//why do you use () and not {} for this function? ****************
+export const KandyKorner = () => (
+    <>
+    <Route
+        render={() => {
+            if (localStorage.getItem("kandy_customer")) {
+                return (
+                <>
+                    <NavBar />
+                    <ApplicationViews />
+                </>
+                );
+            } else {
+                return <Redirect to="/login" />;
+            }
+        }}
+    />
+
+    <Route path="/login">
+        <Login />
+    </Route>
+    <Route path="/register">
+        <Register />
+    </Route>
+    </>
+)
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -20,6 +20,16 @@ export const NavBar = (props) => {
             <li className="navbar__item">
                 <Link className="navbar__link" to="/customers">Customers</Link>
             </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="#"
+                    onClick={
+                        () => {
+                            localStorage.removeItem("kandy_customer")
+                        }
+                    }>
+                    Logout
+                </Link>
+            </li>
         </ul>
     )
 }
